3228ab34fbcd7ea49fd8208b63a2cb863de5e4e4
Samus9001 <sameer99@outlook.com>
2023-02-12 20:30:26 +0000

    added ui prompts and if statements based on user input

--- Program.cs
+++ Program.cs
@@ -6,24 +6,70 @@
         const int MAX_NUMBER = 10;
         static void Main(string[] args)
         {
-
+            //sets the random elements value
             var rnd = new Random();
             int index;
 
-            int[,] slotMachine = new int[3, 3];
+            Console.WriteLine("Welcome to Slots! Enter $1 to play one line, $2 to play two lines, $3 to play three lines\n");
 
-            int rows = slotMachine.GetLength(0);
-            int cols = slotMachine.GetLength(1);
-
-            for (int i = 0; i < rows; i++)
+            while (true)
             {
-                for (int j = 0; j < cols; j++)
+                Console.Write("$");
+                char input = Console.ReadKey().KeyChar;
+                Console.WriteLine("\n");
+
+                if (input != '1' && input != '2' && input != '3')
                 {
-                    index = rnd.Next(MIN_NUMBER,MAX_NUMBER);
-                    slotMachine[i, j] = index;
-                    Console.Write(slotMachine[i, j] + " ");
+                    Console.WriteLine("\nThat is not a valid option.\n");
+                    continue;
                 }
-                Console.WriteLine();
+
+                //creates a 3x3 array
+                int[,] slotMachine = new int[3, 3];
+
+                //checks the length of each dimension of the array
+                int row = slotMachine.GetLength(0);
+                int col = slotMachine.GetLength(1);
+
+                if (input == '1')
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        index = rnd.Next(MIN_NUMBER, MAX_NUMBER);
+                        slotMachine[0, j] = index;
+                        Console.Write(slotMachine[0, j] + " ");
+                    }
+                    Console.WriteLine("\n");
+                }
+
+                if (input == '2')
+                {
+                    
+                }
+
+                if (input == '3')
+                {
+                    //loops through each element in the array and sets the values randomly
+                    for (int i = 0; i < row; i++)
+                    {
+                        for (int j = 0; j < col; j++)
+                        {
+                            index = rnd.Next(MIN_NUMBER, MAX_NUMBER);
+                            slotMachine[i, j] = index;
+                            Console.Write(slotMachine[i, j] + " ");
+                        }
+                        Console.WriteLine("\n");
+                    }
+                }
+
+                /*
+                char restart = Console.ReadKey().KeyChar;
+
+                if (restart == 'r')
+                {
+                    Console.Clear();
+                }
+                */
             }
         }
     }
