bffeacd7a787ec389cb46b4279e4eb3f6384b1e4
Samus9001 <sameer99@outlook.com>
2023-02-12 16:13:30 +0000

    removed list for range of array elements and replaced with constant values. replaced hard coded values used in for loops with variables that use the length of the array dimensions

--- Program.cs
+++ Program.cs
@@ -2,21 +2,24 @@
 {
     internal class Program
     {
+        const int MIN_NUMBER = 0;
+        const int MAX_NUMBER = 10;
         static void Main(string[] args)
         {
-
-            List<int> numbers = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             var rnd = new Random();
             int index;
 
             int[,] slotMachine = new int[3, 3];
 
-            for (int i = 0; i < 3; i++)
+            int rows = slotMachine.GetLength(0);
+            int cols = slotMachine.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < cols; j++)
                 {
-                    index = rnd.Next(numbers.Count);
+                    index = rnd.Next(MIN_NUMBER,MAX_NUMBER);
                     slotMachine[i, j] = index;
                     Console.Write(slotMachine[i, j] + " ");
                 }
