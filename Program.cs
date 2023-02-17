7900d502d611ca37040a0a65aef9951f5c5cf319
Samus9001 <sameer99@outlook.com>
2023-02-17 22:46:03 +0000

    #1 added a check for matching elements in one row

--- Program.cs
+++ Program.cs
@@ -1,23 +1,33 @@
-﻿namespace SlotMachine
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Threading;
+
+namespace SlotMachine
 {
     internal class Program
     {
         const int MAX_NUMBER = 10;
+        const int MAX_LOSS = 3;
         static void Main(string[] args)
         {
             //sets the random elements value
             var rnd = new Random();
             int index;
+            int startingBalance = 3;
+            int balance = startingBalance;
 
-            Console.WriteLine("Welcome to Slots! Enter $1 to play one line, $2 to play two lines, $3 to play three lines\n");
+            Console.WriteLine("Welcome to Slots!\n");
 
             while (true)
             {
+                Console.WriteLine($"Your balance is: ${balance}\n");
+                Console.WriteLine("Enter $1 to play one line or $3 to play all three lines!\n");
                 Console.Write("$");
                 char input = Console.ReadKey().KeyChar;
-                Console.WriteLine("\n");
+                Console.Clear();
 
-                if (input != '1' && input != '2' && input != '3')
+                //checks if input is out of bounds
+                if (input != '1' && input != '3')
                 {
                     Console.WriteLine("\nThat is not a valid option.\n");
                     continue;
@@ -32,50 +42,63 @@
 
                 if (input == '1')
                 {
+                    //loops through each element in the first row of the array and sets the values
                     for (int j = 0; j < cols; j++)
                     {
                         index = rnd.Next(MAX_NUMBER);
                         slotMachine[0, j] = index;
                         Console.Write(slotMachine[0, j] + " ");
                     }
-                    Console.WriteLine("\n");
-                }
 
-                if (input == '2')
-                {
-                    for (int j = 0; j < cols; j++)
+                    //checks if the elements in the first row are matching
+                    if (slotMachine[0, 0] == slotMachine[0, 1] && slotMachine[0, 1] == slotMachine[0, 2])
                     {
-                        index = rnd.Next(MAX_NUMBER);
-                        slotMachine[0, j] = index;
-                        Console.Write(slotMachine[0, j] + " ");
-                        Console.Write(slotMachine[1, j] + " ");
+                        Console.WriteLine("\nYOU WIN!");
+                        balance += 10;
                     }
-                    Console.WriteLine("\n");
-                }
-
-                if (input == '3')
-                {
-                    //loops through each element in the array and sets the values randomly
-                    for (int i = 0; i < rows; i++)
+                    else
                     {
-                        for (int j = 0; j < cols; j++)
+                        balance--;
+                    }
+
+                    if (input == '3')
+                    {
+                        //loops through each element in the array and sets the values
+                        for (int i = 0; i < rows; i++)
                         {
-                            index = rnd.Next(MAX_NUMBER);
-                            slotMachine[i, j] = index;
-                            Console.Write(slotMachine[i, j] + " ");
+                            for (int j = 0; j < cols; j++)
+                            {
+                                index = rnd.Next(MAX_NUMBER);
+                                slotMachine[i, j] = index;
+                                Console.Write(slotMachine[i, j] + " ");
+                            }
+                            Console.WriteLine();
                         }
-                        Console.WriteLine("\n");
+                        //balance = balance - MAX_LOSS;
                     }
-                }
 
-                /*
-                char restart = Console.ReadKey().KeyChar;
+                    if (balance >= 1)
+                    {
+                        Console.WriteLine($"\n\nYour balance is ${balance}\n");
+                    }
 
-                if (restart == 'r')
-                {
-                    Console.Clear();
+                    if (balance == 0)
+                    {
+                        Console.WriteLine($"\n\nYour balance is ${balance}\n");
+                        Console.WriteLine("YOU LOSE!");
+                        balance = startingBalance;
+                    }
+
+                    //prompts user to restart or exit game
+                    Console.WriteLine("If you would like to play again press Y or press any other key to exit");
+                    char endGame = Char.ToUpper(Console.ReadKey().KeyChar);
+
+                    if (endGame == 'Y')
+                    {
+                        Console.Clear();
+                    }
+                    else return;
                 }
-                */
             }
         }
     }
