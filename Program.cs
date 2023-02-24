31b449ee4c267b19b12cf5dbc4c3abe42d459429
Samus9001 <sameer99@outlook.com>
2023-02-24 22:49:23 +0000

    flow control and ui prompts amended

--- Program.cs
+++ Program.cs
@@ -13,15 +13,33 @@ namespace SlotMachine
             //sets the random elements value
             var rnd = new Random();
             int index;
-            int startingBalance = 3;
+            int startingBalance = 100;
             int balance = startingBalance;
+            bool endGame = false;
+            char restartGame = 'N';
 
-            Console.WriteLine("Welcome to Slots!\n");
+            //creates a 3x3 array
+            int[,] slotMachine = new int[3, 3];
+
+            //checks the length of each dimension of the array
+            int rows = slotMachine.GetLength(0);
+            int cols = slotMachine.GetLength(1);
+
+            Console.WriteLine("Welcome to Slots!\n\n");
 
             while (true)
             {
-                Console.WriteLine($"Your balance is: ${balance}\n");
-                Console.WriteLine("Enter $1 to play one line or $3 to play all three lines!\n");
+                if (balance >= 3)
+                {
+                    Console.WriteLine("Enter $1 to play one line or $3 to play all three lines!\n\nPress any other key to leave with your balance");
+                }
+
+                if (balance > 0 && balance < 3)
+                {
+                    Console.WriteLine("Enter $1 to play one line!\n\nPress any other key to leave with your balance");
+                }
+
+                Console.WriteLine($"\n\nYour balance is ${balance}\n");
                 Console.Write("$");
                 char input = Console.ReadKey().KeyChar;
                 Console.Clear();
@@ -29,16 +47,9 @@ namespace SlotMachine
                 //checks if input is out of bounds
                 if (input != '1' && input != '3')
                 {
-                    Console.WriteLine("\nThat is not a valid option.\n");
+                    Console.WriteLine("\n\nThat is not a valid option.\n");
                     continue;
                 }
-
-                //creates a 3x3 array
-                int[,] slotMachine = new int[3, 3];
-
-                //checks the length of each dimension of the array
-                int rows = slotMachine.GetLength(0);
-                int cols = slotMachine.GetLength(1);
 
                 if (input == '1')
                 {
@@ -49,6 +60,7 @@ namespace SlotMachine
                         slotMachine[0, j] = index;
                         Console.Write(slotMachine[0, j] + " ");
                     }
+                    Console.WriteLine();
 
                     //checks if the elements in the first row are matching
                     if (slotMachine[0, 0] == slotMachine[0, 1] && slotMachine[0, 1] == slotMachine[0, 2])
@@ -60,44 +72,49 @@ namespace SlotMachine
                     {
                         balance--;
                     }
+                }
 
-                    if (input == '3')
+                if (input == '3')
+                {
+                    //loops through each element in the array and sets the values
+                    for (int i = 0; i < rows; i++)
                     {
-                        //loops through each element in the array and sets the values
-                        for (int i = 0; i < rows; i++)
+                        for (int j = 0; j < cols; j++)
                         {
-                            for (int j = 0; j < cols; j++)
-                            {
-                                index = rnd.Next(MAX_NUMBER);
-                                slotMachine[i, j] = index;
-                                Console.Write(slotMachine[i, j] + " ");
-                            }
-                            Console.WriteLine();
+                            index = rnd.Next(MAX_NUMBER);
+                            slotMachine[i, j] = index;
+                            Console.Write(slotMachine[i, j] + " ");
                         }
-                        //balance = balance - MAX_LOSS;
+                        Console.WriteLine();
                     }
+                    balance = balance - MAX_LOSS;
+                }
+                Console.WriteLine("\n");
 
-                    if (balance >= 1)
-                    {
-                        Console.WriteLine($"\n\nYour balance is ${balance}\n");
-                    }
-
-                    if (balance == 0)
-                    {
-                        Console.WriteLine($"\n\nYour balance is ${balance}\n");
-                        Console.WriteLine("YOU LOSE!");
-                        balance = startingBalance;
-                    }
-
-                    //prompts user to restart or exit game
+                if (balance <= 0)
+                {
+                    Console.WriteLine("\nYOU LOSE!\n");
                     Console.WriteLine("If you would like to play again press Y or press any other key to exit");
-                    char endGame = Char.ToUpper(Console.ReadKey().KeyChar);
+                    endGame = true;
+                }
 
-                    if (endGame == 'Y')
-                    {
-                        Console.Clear();
-                    }
-                    else return;
+
+                if (endGame)
+                {
+                    restartGame = Console.ReadKey().KeyChar;
+                }
+
+                if (restartGame == 'Y')
+                {
+                    balance = startingBalance;
+                    Console.Clear();
+                    endGame = false;
+                    restartGame = 'N';
+
+                }
+                else if (endGame)
+                {
+                    return;
                 }
             }
         }
